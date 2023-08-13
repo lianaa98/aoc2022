d832895ab66d3916e097aae44711d3eaf7d26884
lianaa98 <liana.kan98@gmail.com>
2023-08-13 13:49:22 -0700

    new solution for day2 part1

--- day2/part1.js
+++ day2/part1.js
@@ -7,44 +7,56 @@ Draw = 3 points;
 Won = 6 points;
 */
 
-let fs = require('fs');
-let array = fs.readFileSync('input.txt').toString().split("\n");
-let outcomes = [];
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf-8')
+const rounds = data.split("\n");
 
-for (let i of array) {
-  let output = i.toString().split(" ");
-  outcomes.push(output);
-}
-console.log(outcomes);
-
-let score = 0;
-
-for (let num of outcomes) {
-if(num[1] === 'X') {
-  if(num[0] === 'C') {
-    score += (6 + 1);
-  } else if(num[0] === 'B') {
-    score += (0 + 1);
-  } else if(num[0] === 'A') {
-    score += (3 + 1);
-  }
-} else if(num[1] === 'Y') {
-  if(num[0] === 'C') {
-    score += (0 + 2);
-  } else if(num[0] === 'A') {
-    score += (6 + 2);
-  } else if(num[0] === 'B') {
-    score += (3 + 2);
-  }
-} else if(num[1] === 'Z') {
-  if(num[0] === 'A') {
-    score += (0 + 3);
-  } else if(num[0] === 'B') {
-    score += (6 + 3);
-  } else if(num[0] === 'C') {
-    score += (3 + 3);
-  }
-}
+const myPoints = {
+  X: 1,
+  Y: 2,
+  Z: 3 
 }
 
-console.log(score);
+const opponentPoints = {
+  A: 1,
+  B: 2,
+  C: 3
+}
+
+const winningPoints = {
+  lost: 0,
+  draw: 3,
+  won: 6
+}
+
+const myWinningPointsThisRound = (myChoice, opponentChoice) => {
+  if (myPoints[myChoice] === 3 && opponentPoints[opponentChoice] === 1) {
+    return winningPoints.lost;
+  }
+  if (myPoints[myChoice] === 1 && opponentPoints[opponentChoice] === 3) {
+    return winningPoints.won;
+  }
+  if (myPoints[myChoice] === opponentPoints[opponentChoice]) {
+    return winningPoints.draw;
+  } 
+  if (myPoints[myChoice] > opponentPoints[opponentChoice]) {
+    return winningPoints.won;
+  }
+  if (myPoints[myChoice] < opponentPoints[opponentChoice]) {
+    return winningPoints.lost;
+  }
+}
+
+let myTotalPoints = 0;
+
+const outcomes = rounds.map((round) => {
+  const myChoice = round[2];
+  const opponentChoice = round[0];
+
+  const myWinningPoints = myWinningPointsThisRound(myChoice, opponentChoice);
+  const myChoicePoints = myPoints[myChoice];
+
+  myTotalPoints += myWinningPoints + myChoicePoints;
+})
+
+console.log(myTotalPoints);
