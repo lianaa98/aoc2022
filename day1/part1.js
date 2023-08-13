5d22b0b9c53bbf54c63a30c01430e2ba50b38e1d
lianaa98 <liana.kan98@gmail.com>
2023-08-13 13:08:48 -0700

    optimize old code

--- day1/part1.js
+++ day1/part1.js
@@ -1,22 +1,11 @@
-let fs = require('fs');
-let array = fs.readFileSync('input.txt').toString().split("\n\n");
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', 'utf-8');
+const groups = data.split("\n\n");
 
-let elves = [];
+const elves = groups.map(group => {
+  const values = group.trim().split("\n").map(Number);
+  return values.reduce((sum, value) => sum + value, 0);
+});
 
-
-for (let i of array) {
-
-  if (i.includes("\n")) {
-    let elf = i.toString().split("\n");
-    let eachElf = 0;
-    for (let num = 0; num < elf.length; num++) {
-      eachElf = Number(eachElf) + Number(elf[num]);
-    }
-    elves.push(eachElf);
-  } else if (!(i.includes("\n"))) {
-    elves.push(Number(i));
-  } 
-
-}
-
-console.log(Math.max(...elves));
+const maxElf = Math.max(...elves);
+console.log(maxElf);
