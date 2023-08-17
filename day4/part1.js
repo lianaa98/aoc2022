44e2edbdf32c1a324c9fb84fc383e85da82eb3f3
lianaa98 <liana.kan98@gmail.com>
2023-08-17 14:15:03 -0700

    optimize day4 part1

--- day4/part1.js
+++ day4/part1.js
@@ -4,8 +4,6 @@ const pairs = data.split("\n");
 
 let totalPairsWhereOneContainsTheOther = 0;
 
-const compareArrays = (arrayOne, arrayTwo) => arrayOne.length === arrayTwo.length && arrayOne.every((element, index) => element === arrayTwo[index]);
-
 for (const pair of pairs) {
   const sectionOne = pair.split(",")[0];
   const sectionTwo = pair.split(",")[1];
@@ -15,22 +13,9 @@ for (const pair of pairs) {
   const sectionTwoStartNum = Number(sectionTwo.split("-")[0]);
   const sectionTwoEndNum = Number(sectionTwo.split("-")[1]);
 
-  const sectionOneArray = [];
-  const sectionTwoArray = [];
-
-  for (let i = sectionOneStartNum; i <= sectionOneEndNum; i++) {
-    sectionOneArray.push(i);
-  }
-  for (let i = sectionTwoStartNum; i <= sectionTwoEndNum; i++) {
-    sectionTwoArray.push(i);
-  }
-
-  const filteredArrayOne = sectionOneArray.filter(num => sectionTwoArray.includes(num));
-  const filteredArrayTwo = sectionTwoArray.filter(num => sectionOneArray.includes(num));
-
   if (
-    compareArrays(filteredArrayOne, sectionOneArray) ||
-    compareArrays(filteredArrayTwo, sectionTwoArray)
+    (sectionOneStartNum >= sectionTwoStartNum && sectionOneEndNum <= sectionTwoEndNum) ||
+    (sectionTwoStartNum >= sectionOneStartNum && sectionTwoEndNum <= sectionOneEndNum)
   ) {
     totalPairsWhereOneContainsTheOther += 1;
   }
